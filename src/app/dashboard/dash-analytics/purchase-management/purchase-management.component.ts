fa71bb37e87105ec9afcaa0368e83b79121f1362
Gokul GK <gokul@greencirclehealth.com>
2020-06-27 11:19:57 +0530

    Purchase Date filter

--- src/app/dashboard/dash-analytics/purchase-management/purchase-management.component.ts
+++ src/app/dashboard/dash-analytics/purchase-management/purchase-management.component.ts
@@ -2,23 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import * as moment from 'moment';
 
-export interface PeriodicElement {
+export interface PurchaseElement {
   id: number;
   name: string;
   course: string;
-  email:string;
+  email: string;
   phonenumber: number;
   paidamount: number;
   paidon: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 1, name: 'Hydrogen',course:'java',email:'kavi@gmail',phonenumber: 907856435673,paidamount: 500, paidon:'22/04/1996'},
-  {id: 2, name: 'Hello',course:'c++',email:'kavi@gmail',phonenumber: 907856435673,paidamount: 500, paidon:'22/03/1996'},
-  {id: 3, name: 'shyam',course:'C',email:'kavi@gmail',phonenumber: 907856435673,paidamount: 500, paidon:'22/02/1996'},
-  {id: 4, name: 'Man',course:'java',email:'kavi@gmail',phonenumber: 907856435673,paidamount: 500, paidon:'22/03/1996'},
-  {id: 5, name: 'amuthan',course:'Digital',email:'kavi@gmail',phonenumber: 907856435673,paidamount: 500, paidon:'20/04/1996'},
-  {id: 6, name: 'kavitha',course:'java',email:'kavi@gmail',phonenumber: 907856435673,paidamount: 500, paidon:'25/04/1996'},
-  {id: 7, name: 'Hydro',course:'java',email:'kavi@gmail',phonenumber: 907856435673,paidamount: 500, paidon:'26/04/1996'},
+const ELEMENT_DATA: PurchaseElement[] = [
+  { id: 1, name: 'Hydrogen', course: 'java', email: 'kavi@gmail', phonenumber: 907856435673, paidamount: 500, paidon: '22/04/2020' },
+  { id: 2, name: 'Hello', course: 'c++', email: 'kavi@gmail', phonenumber: 907856435673, paidamount: 500, paidon: '14/04/2020' },
+  { id: 3, name: 'shyam', course: 'C', email: 'kavi@gmail', phonenumber: 907856435673, paidamount: 500, paidon: '11/02/2020' },
+  { id: 4, name: 'Man', course: 'java', email: 'kavi@gmail', phonenumber: 907856435673, paidamount: 500, paidon: '02/03/2020' },
+  { id: 5, name: 'amuthan', course: 'Digital', email: 'kavi@gmail', phonenumber: 907856435673, paidamount: 500, paidon: '20/12/2019' },
+  { id: 6, name: 'kavitha', course: 'java', email: 'kavi@gmail', phonenumber: 907856435673, paidamount: 500, paidon: '25/11/2019' },
+  { id: 7, name: 'Hydro', course: 'java', email: 'kavi@gmail', phonenumber: 907856435673, paidamount: 500, paidon: '26/12/2019' },
 ];
 
 @Component({
@@ -27,28 +27,39 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./purchase-management.component.scss']
 })
 export class PurchaseManagementComponent implements OnInit {
-  fromDate: Date;
-  toDate: Date;
+  fromDate: Date = null;
+  toDate: Date = null;
 
-  displayedColumns: string[] = ['id', 'name', 'course', 'email','phonenumber','paidamount','paidon'];
+  displayedColumns: string[] = ['id', 'name', 'course', 'email', 'phonenumber', 'paidamount', 'paidon'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-  filter()
-  {
-    console.log(moment(this.fromDate).format('DD/MM/YYYY'));
-    console.log(moment(this.toDate).format('DD/MM/YYYY'));
-    // console.log(new Date("26-04-1996"))
-    // console.log(this.fromDate)
-    // console.log(this.toDate)
-  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  // }
+
+  filter() {
+    let filteredData: PurchaseElement[] = [];
+    const fromDate = this.fromDate ?
+      moment(moment(this.fromDate).format('DD/MM/YYYY'), 'DD/MM/YYYY') : null;
+    const toDate = this.toDate ?
+      moment(moment(this.toDate).format('DD/MM/YYYY'), 'DD/MM/YYYY') : null;
+    if (fromDate && toDate) {
+      ELEMENT_DATA.forEach(element => {
+        const paidOn = moment(element.paidon, "DD/MM/YYYY");
+        if (paidOn && paidOn.isBetween(fromDate, toDate)) {
+          filteredData.push(element);
+        }
+      });
+      this.dataSource = new MatTableDataSource(filteredData);
+    } else {
+      this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+    }
   }
 
 }
