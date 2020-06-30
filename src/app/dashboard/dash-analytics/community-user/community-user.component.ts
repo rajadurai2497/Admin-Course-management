cc2f5a595061bfd885c4b3c5a00ca33844fb5559
Beena2104 <beenadasc@gmail.com>
2020-06-30 09:58:46 +0530

    admin intergation

--- src/app/dashboard/dash-analytics/community-user/community-user.component.ts
+++ src/app/dashboard/dash-analytics/community-user/community-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommunityUserService } from 'src/app/services/community-user.service';
+import { CommunityUserModel } from 'src/app/models/community-user.model';
 
 @Component({
   selector: 'app-community-user',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./community-user.component.scss']
 })
 export class CommunityUserComponent implements OnInit {
+  publiccommunityUser: CommunityUserModel;
+  displayedColumns: string[];
+  communityUser: CommunityUserModel;
 
-  constructor() { }
+  constructor(private readonly _communityUserService: CommunityUserService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this._initializeValues();
+    this._initializeProperties();
+    this.getAllCommunityUser();
+  }
+
+  public getAllCommunityUser(): void {
+    this._communityUserService.getAllCommunityUser().subscribe((data: CommunityUserModel) => {
+      this.communityUser = data;
+    });
+  }
+
+
+  private _initializeValues(): void {
+    this.communityUser = {
+      comunityUserList: [],
+      result: true,
+    };
+  }
+
+  private _initializeProperties(): void {
+    this.displayedColumns = ['id', 'fullName', 'emailId', 'phoneNumber'];
   }
 
 }
--- src/app/dashboard/dash-analytics/purchase-management/purchase-management.module.ts
+++ src/app/dashboard/dash-analytics/purchase-management/purchase-management.module.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PurchaseManagementRoutingModule } from './purchase-management-routing.module';
 import { PurchaseManagementComponent } from './purchase-management.component';
+import {
+  MatDialogModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSlideToggleModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+} from '@angular/material';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 
 
@@ -12,6 +21,8 @@ import { SharedModule } from 'src/app/theme/shared/shared.module';
     CommonModule,
     PurchaseManagementRoutingModule,
     SharedModule,
+    MatTableModule, MatIconModule, MatButtonModule
+
   ]
 })
 export class PurchaseManagementModule { }
