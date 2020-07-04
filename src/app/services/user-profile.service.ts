1848d4f7bf6843b0c174409b1d63468bfff91dc6
rajadurai2497 <rajadurai2497@gmail.com>
2020-07-04 10:57:26 +0530

    profile-card

---
+++ src/app/services/user-profile.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Injector } from '@angular/core';
+import { ROUTE_CONFIG } from '../models/constant';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserProfileService extends ApiService{
+
+  constructor(private readonly injector: Injector) {
+    super(injector);
+
+  }
+
+  getUserProfile(): Promise<any> {
+    const apiUrl = ROUTE_CONFIG.BaseUrl + `/Token/GetMyProfile`;
+    return this.doGet(apiUrl, true);
+  }
+}
