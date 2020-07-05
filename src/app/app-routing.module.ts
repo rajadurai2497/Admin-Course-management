54ad8de8d88f001d889a23da372cad1ac063cada
Albin Lijo <albinlijo1997@gmail.com>
2020-07-05 21:47:29 +0530

    Fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,29 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './theme/layout/admin/admin.component';
 import { AuthComponent } from './theme/layout/auth/auth.component';
-
+import { AuthGuardGuard } from './theme/shared/guard/auth-guard.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
+    canActivate: [AuthGuardGuard],
+    canLoad: [AuthGuardGuard],
     children: [
       {
         path: '',
         redirectTo: '/auth/signin',
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'dashboard',
-        loadChildren: () => import('./dashboard/dashboard.module').then(module => module.DashboardModule)
+        loadChildren: () => import('./dashboard/dashboard.module').then((module) => module.DashboardModule),
       },
       {
         path: 'users',
-        loadChildren: () => import('./users/users.module').then(module => module.UsersModule)
+        loadChildren: () => import('./users/users.module').then((module) => module.UsersModule),
       },
-    ]
+    ],
   },
   {
     path: '',
@@ -30,18 +32,19 @@ const routes: Routes = [
     children: [
       {
         path: 'auth',
-        loadChildren: () => import('./authentication/authentication.module').then(module => module.AuthenticationModule)
+        loadChildren: () =>
+          import('./authentication/authentication.module').then((module) => module.AuthenticationModule),
       },
       {
         path: 'maintenance',
-        loadChildren: () => import('./maintenance/maintenance.module').then(module => module.MaintenanceModule)
+        loadChildren: () => import('./maintenance/maintenance.module').then((module) => module.MaintenanceModule),
       },
-    ]
+    ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
